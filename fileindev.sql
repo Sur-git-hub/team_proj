34e653e768fff1e56731b6339126019b52a6e445
Sur-git-hub <sury.6000@gmail.com>
2024-08-27 16:52:04 +0530

    file copied from dev branch to work with

---
+++ fileindev.sql
@@ -0,0 +1,30 @@
+WITH sales_per_car AS (
+    SELECT c.id,
+         c.car_make,
+         c.model,
+           c.motor_type,
+           c.year_of_production,
+           AVG(cs.sales)::INT AS average_sales_2022
+    FROM cars c JOIN car_sales cs ON c.id = cs.cars_id
+    WHERE c.year_of_production = 2022
+    GROUP BY c.id, c.car_make, c.model, c.motor_type, c.year_of_production
+),
+ 
+budget AS (
+    SELECT *,
+         average_sales_2022 * 12 AS annual_planned_sales_2022
+    FROM sales_per_car
+)
+     
+SELECT b.car_make,
+     b.model,
+     b.motor_type,
+     b.year_of_production,
+     SUM(cs.sales) AS actual_ytd_sales_2022,
+     b.annual_planned_sales_2022,
+     b.annual_planned_sales_2022 - SUM(cs.sales) AS remaining_annual_sales_2022
+FROM budget b
+JOIN car_sales cs
+ON b.id = cs.cars_id
+GROUP BY b.car_make, b.model, b.motor_type, b.year_of_production, b.annual_planned_sales_2022;
+--added to my branch
