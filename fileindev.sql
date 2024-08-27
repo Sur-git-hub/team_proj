9ba9928c5955f9c51140ccf6ab59aa6b26ff81c1
Shree Raksha Sivasubramani <74097590+Rakshu2141@users.noreply.github.com>
2024-08-27 08:19:10 -0400

    Update fileindev.sql
    Made Changes in cte name

--- fileindev.sql
+++ fileindev.sql
@@ -10,7 +10,7 @@ WITH sales_per_car AS (
     GROUP BY c.id, c.car_make, c.model, c.motor_type, c.year_of_production
 ),
  
-budget AS (
+budget_per_annually AS (
     SELECT *,
          average_sales_2022 * 12 AS annual_planned_sales_2022
     FROM sales_per_car
@@ -23,7 +23,7 @@ SELECT b.car_make,
      SUM(cs.sales) AS actual_ytd_sales_2022,
      b.annual_planned_sales_2022,
      b.annual_planned_sales_2022 - SUM(cs.sales) AS remaining_annual_sales_2022
-FROM budget b
+FROM budget_per_annually b
 JOIN car_sales cs
 ON b.id = cs.cars_id
 GROUP BY b.car_make, b.model, b.motor_type, b.year_of_production, b.annual_planned_sales_2022;
