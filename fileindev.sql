2cacdfcba3db15914e919fa586aea61821c7b0d4
Jayagandan16 <jayagandanmurugesan16@gmail.com>
2024-08-29 15:35:26 +0530

    update the year

--- fileindev.sql
+++ fileindev.sql
@@ -6,7 +6,7 @@ WITH sales_per_car AS (
            c.year_of_production,
            AVG(cs.sales)::INT AS a
     FROM cars c JOIN car_sales cs ON c.id = cs.cars_id
-    WHERE c.year_of_production = 2022
+    WHERE c.year_of_production = 2024
     GROUP BY c.id, c.car_make, c.model, c.motor_type, c.year_of_production
 ),
  
@@ -22,7 +22,7 @@ SELECT b.car_make,
      b.year_of_production,
      SUM(cs.sales) AS actual_ytd_sales_2022,
      b.annual_planned_sales_2024,
-     b.annual_planned_sales_2024 - SUM(cs.sales) AS remaining_annual_sales_2022
+     b.annual_planned_sales_2024 - SUM(cs.sales) AS remaining_annual_sales_2024
 FROM budget_per_annually b
 JOIN car_sales cs
 ON b.id = cs.cars_id
