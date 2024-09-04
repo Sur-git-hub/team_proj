401c4e3de43de43272dd6a87cca65235ef733fd6
Rakshu2141 <shreerakshu01@gmail.com>
2024-09-04 05:26:59 -0400

    Added a line

--- fileindev.sql
+++ fileindev.sql
@@ -9,9 +9,11 @@ WITH sales_per_car AS (
     WHERE c.year_of_production = 2020
     GROUP BY c.id, c.car_make, c.model, c.motor_type, c.year_of_production
 ),
+
 test_cte AS (
      SELECT * FROM sales_per_car WHERE c.id > 1;
 )
+
 budget_per_annually AS (
     SELECT *,
          average_sales_2024 * 12 AS annual_planned_sales_2020
@@ -29,3 +31,5 @@ FROM budget_per_annually b
 JOIN car_sales cs
 ON b.id = cs.cars_id
 GROUP BY b.car_make, b.model, b.motor_type, b.year_of_production, b.annual_planned_sales_2020;
+--- This statement shows the values of Car Sales per annually where year is = 2020
+
