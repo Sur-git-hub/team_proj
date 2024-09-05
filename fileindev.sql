ea72f7a826ef318f010f24be6aaf2cc939ef303c
Jayagandan16 <jayagandanmurugesan16@gmail.com>
2024-09-05 19:21:33 +0530

    i made the changes on year

--- fileindev.sql
+++ fileindev.sql
@@ -9,7 +9,7 @@ WITH sales_per_car AS (
     WHERE c.year_of_production = 2020
     GROUP BY c.id, c.car_make, c.model, c.motor_type, c.year_of_production
 ),
- 
+ ------yearly production 2020---
 budget_per_annually AS (
     SELECT *,
          average_sales_2024 * 12 AS annual_planned_sales_2020
@@ -20,10 +20,10 @@ SELECT b.car_make,
      b.model,
      b.motor_type,
      b.year_of_production,
-     SUM(cs.sales) AS actual_ytd_sales_2020,
+     SUM(cs.sales) AS actual_ytd_sales_2022,
      b.annual_planned_sales_2020,
-     b.annual_planned_sales_2020 - SUM(cs.sales) AS remaining_annual_sales_2020
+     b.annual_planned_sales_2020 - SUM(cs.sales) AS remaining_annual_sales_2022
 FROM budget_per_annually b
 JOIN car_sales cs
 ON b.id = cs.cars_id
-GROUP BY b.car_make, b.model, b.motor_type, b.year_of_production, b.annual_planned_sales_2020;
+GROUP BY b.car_make, b.model, b.motor_type, b.year_of_production, b.annual_planned_sales_2022;
